50a52216c0dfac9f40d14ed5c82afe4b5a8f2265
CodieMisiani <nimrodmisiani1@gmail.com>
2025-06-01 10:03:50 +0300

    Refactor main function to include personalized greeting and age display

--- playground.dart
+++ playground.dart
@@ -1,3 +1,23 @@
+// void main() {
+//   print('Nimrod Misiani');
+// }
+
+// void main() {
+//   int age = 20;
+//   String name = "Nimrod Misiani";
+//   double height = 1.75;
+//   bool isStudent = true;
+
+//   print("Name: $name");
+//   print("Age: $age");
+//   print("Height: $height m");
+//   print("Is Student: $isStudent");
+// }
+
 void main() {
-  print('Nimrod Misiani');
+  var name = "Nimrod Misiani";
+  print("Hello, $name!");
+
+  dynamic age = 20;
+  print("Your age is $age years old.");
 }
